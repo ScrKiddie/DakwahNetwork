cef878ba59e4b2d5d1e0f32801ab8d344fad2600
scrkiddie <hilmitrial4@gmail.com>
2024-05-27 19:41:23 +0700

    Menambahkan page visitor (beranda,dakwah,contact)

---
+++ app/Views/Visitor/dakwah.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr" class="landing-pages"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>DakNet | Dakwah Network</title>
+    <link rel="shortcut icon" href="<?=base_url()?>img/logo.png" />
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/core/libs.min.css" />
+    <link rel="stylesheet" href="<?=base_url()?>assets/vendor/aos/dist/aos.css" />
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/hope-ui.min.css?v=2.0.0" />
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/custom.min.css?v=2.0.0" />
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/dark.min.css"/>
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/customizer.min.css" />
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/rtl.min.css"/>
+
+    <style>
+        .sub-header {
+            bac
+            background-repeat: no-repeat;
+            padding: 100px 0px;
+            background-color: #3a57e8;
+            -webkit-background-size: cover;
+            background-size: cover;
+            color: #fff
+        }
+        ::-webkit-scrollbar {
+            width: 6px;
+            height: 6px;
+        }
+
+        ::-webkit-scrollbar-track {
+            border-radius: 10px;
+        }
+        ::-webkit-scrollbar-thumb {
+            background: #4b68f9;
+            border-radius: 10px;
+        }
+        ::-webkit-scrollbar-thumb:hover {
+            background: #3a57e8;
+        }
+
+    </style>
+
+    <!-- SwiperSlider css -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/landing-pages.min.css">
+    <style class="fslightbox-styles">.fslightbox-absoluted{position:absolute;top:0;left:0}.fslightbox-fade-in{animation:fslightbox-fade-in .3s cubic-bezier(0,0,.7,1)}.fslightbox-fade-out{animation:fslightbox-fade-out .3s ease}.fslightbox-fade-in-strong{animation:fslightbox-fade-in-strong .3s cubic-bezier(0,0,.7,1)}.fslightbox-fade-out-strong{animation:fslightbox-fade-out-strong .3s ease}@keyframes fslightbox-fade-in{from{opacity:.65}to{opacity:1}}@keyframes fslightbox-fade-out{from{opacity:.35}to{opacity:0}}@keyframes fslightbox-fade-in-strong{from{opacity:.3}to{opacity:1}}@keyframes fslightbox-fade-out-strong{from{opacity:1}to{opacity:0}}.fslightbox-cursor-grabbing{cursor:grabbing}.fslightbox-full-dimension{width:100%;height:100%}.fslightbox-open{overflow:hidden;height:100%}.fslightbox-flex-centered{display:flex;justify-content:center;align-items:center}.fslightbox-opacity-0{opacity:0!important}.fslightbox-opacity-1{opacity:1!important}.fslightbox-scrollbarfix{padding-right:17px}.fslightbox-transform-transition{transition:transform .3s}.fslightbox-container{font-family:Arial,sans-serif;position:fixed;top:0;left:0;background:linear-gradient(rgba(30,30,30,.9),#000 1810%);touch-action:pinch-zoom;z-index:1000000000;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;-webkit-tap-highlight-color:transparent}.fslightbox-container *{box-sizing:border-box}.fslightbox-svg-path{transition:fill .15s ease;fill:#ddd}.fslightbox-nav{height:45px;width:100%;position:absolute;top:0;left:0}.fslightbox-slide-number-container{display:flex;justify-content:center;align-items:center;position:relative;height:100%;font-size:15px;color:#d7d7d7;z-index:0;max-width:55px;text-align:left}.fslightbox-slide-number-container .fslightbox-flex-centered{height:100%}.fslightbox-slash{display:block;margin:0 5px;width:1px;height:12px;transform:rotate(15deg);background:#fff}.fslightbox-toolbar{position:absolute;z-index:3;right:0;top:0;height:100%;display:flex;background:rgba(35,35,35,.65)}.fslightbox-toolbar-button{height:100%;width:45px;cursor:pointer}.fslightbox-toolbar-button:hover .fslightbox-svg-path{fill:#fff}.fslightbox-slide-btn-container{display:flex;align-items:center;padding:12px 12px 12px 6px;position:absolute;top:50%;cursor:pointer;z-index:3;transform:translateY(-50%)}@media (min-width:476px){.fslightbox-slide-btn-container{padding:22px 22px 22px 6px}}@media (min-width:768px){.fslightbox-slide-btn-container{padding:30px 30px 30px 6px}}.fslightbox-slide-btn-container:hover .fslightbox-svg-path{fill:#f1f1f1}.fslightbox-slide-btn{padding:9px;font-size:26px;background:rgba(35,35,35,.65)}@media (min-width:768px){.fslightbox-slide-btn{padding:10px}}@media (min-width:1600px){.fslightbox-slide-btn{padding:11px}}.fslightbox-slide-btn-container-previous{left:0}@media (max-width:475.99px){.fslightbox-slide-btn-container-previous{padding-left:3px}}.fslightbox-slide-btn-container-next{right:0;padding-left:12px;padding-right:3px}@media (min-width:476px){.fslightbox-slide-btn-container-next{padding-left:22px}}@media (min-width:768px){.fslightbox-slide-btn-container-next{padding-left:30px}}@media (min-width:476px){.fslightbox-slide-btn-container-next{padding-right:6px}}.fslightbox-down-event-detector{position:absolute;z-index:1}.fslightbox-slide-swiping-hoverer{z-index:4}.fslightbox-invalid-file-wrapper{font-size:22px;color:#eaebeb;margin:auto}.fslightbox-video{object-fit:cover}.fslightbox-youtube-iframe{border:0}.fslightbox-loader{display:block;margin:auto;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);width:67px;height:67px}.fslightbox-loader div{box-sizing:border-box;display:block;position:absolute;width:54px;height:54px;margin:6px;border:5px solid;border-color:#999 transparent transparent transparent;border-radius:50%;animation:fslightbox-loader 1.2s cubic-bezier(.5,0,.5,1) infinite}.fslightbox-loader div:nth-child(1){animation-delay:-.45s}.fslightbox-loader div:nth-child(2){animation-delay:-.3s}.fslightbox-loader div:nth-child(3){animation-delay:-.15s}@keyframes fslightbox-loader{0%{transform:rotate(0)}100%{transform:rotate(360deg)}}.fslightbox-source{position:relative;z-index:2;opacity:0}</style></head>
+<body class=" body-bg landing-pages">
+<span class="screen-darken"></span>
+<!-- loader-Start -->
+<div id="loading">
+    <div class="loader simple-loader animate__animated animate__fadeOut d-none">
+        <div class="loader-body">
+        </div>
+    </div>    </div>
+<!-- loader-END -->
+<!-- Wrapper-start -->
+<main class="main-content">
+    <div class="position-relative">
+        <!-- Nav-Start -->
+        <nav class="nav navbar navbar-expand-xl navbar-light iq-navbar header-hover-menu">
+            <div class="container-fluid navbar-inner">
+                <div class="d-flex align-items-center justify-content-between w-100 landing-header">
+                    <a href="../landing-pages/index.html" class="navbar-brand m-0 d-xl-flex d-none">
+                        <!--Logo start-->
+                        <img width="32px" src="<?=base_url()?>img/logo.png" alt="">
+                        <!--logo End--><h5  style="margin-left:5px" class="logo-title">DakNet</h5>
+                    </a>
+                    <div class="d-flex align-items-center d-xl-none">
+                        <button data-trigger="navbar_main" class="d-xl-none btn btn-primary rounded-pill p-1 pt-0" type="button">
+                            <svg width="20px" class="icon-20" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M4,11V13H16L10.5,18.5L11.92,19.92L19.84,12L11.92,4.08L10.5,5.5L16,11H4Z"></path>
+                            </svg>
+                        </button>
+
+                        <a href="../landing-pages/index.html" class="navbar-brand ms-3  d-xl-none">
+                            <!--Logo start-->
+                            <img width="32px" src="<?=base_url()?>img/logo.png" alt="">
+                            <!--logo End--><h5  style="margin-left:5px" class="logo-title">DakNet</h5>
+                        </a>
+                    </div>
+
+
+                    <!-- Horizontal Menu Start -->
+                    <nav id="navbar_main" class="mobile-offcanvas nav navbar navbar-expand-xl hover-nav horizontal-nav">
+                        <div class="container-fluid p-lg-0">
+                            <div class="offcanvas-header px-0">
+                                <a href="../landing-pages/index.html" class="navbar-brand ms-3  d-xl-none">
+                                    <!--Logo start-->
+                                    <!--Logo start-->
+                                    <img width="32px" src="<?=base_url()?>img/logo.png" alt="">
+                                    <!--logo End--><h5  style="margin-left:5px" class="logo-title">DakNet</h5>
+                                </a>
+                                <button class="btn-close float-end px-3"></button>
+                            </div>
+                            <ul class="navbar-nav iq-nav-menu  list-unstyled" id="header-menu">
+                                <li class="nav-item"><a class="nav-link " href="../landing-pages/about.html">Beranda</a></li>
+                                <li class="nav-item"><a class="nav-link " href="../landing-pages/feature.html">Dakwah</a></li>
+                                <li class="nav-item"><a class="nav-link " href="../landing-pages/contact-us.html">Contact Us</a></li>
+                            </ul>
+                        </div> <!-- container-fluid.// -->
+                    </nav>
+                    <!-- Sidebar Menu End -->
+                </div>
+            </div>
+        </nav>        <!--Nav-End-->
+    </div>
+    <div class="sub-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h3 class="mb-4 text-white">Dakwah</h3>
+                    <h6 class="text-white">Kajian yang sedang berlangsung</h6>
+                </div>
+            </div>
+        </div>
+    </div><div class="inner-card-box">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="card">
+                        <div class="card-body p-3">
+                            <img src="<?=base_url()?>upload/6652bf9f510be.png" alt="" class="img-fluid rounded
+                        " loading="lazy">
+                            <p class="text-primary pt-3">December 26, 2022</p>
+                            <a href="https://templates.iqonic.design/hope-ui/html/dist/landing-pages/blog-detail.html" class="my-3 h5">The Cheapest Destinations Of All Time, A List Of Beauty And Budget.</a>
+                            <div class="d-flex align-items-center my-3">
+                                <span>Travel</span>
+                                <span class="ms-3 text-primary">Admin</span>
+                            </div>
+                            <p class="">Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting Industry. Lorem Ipsum Has Been The Industry’s Standard Dummy Text Ever Since The 1500s, When An Unknown Printer Took A Galley Of Type And Scrambled It To Make A Type Specimen Book.</p>
+                            <a href="https://templates.iqonic.design/hope-ui/html/dist/landing-pages/blog-detail.html" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</main>
+<!-- Wrapper-End -->
+<!-- Footer-start -->
+<footer >
+    <div class="bg-secondary inner-box" >
+        <div class="container">
+            <div class="row" >
+                <div class="col-md-4 mt-md-0 mt-4">
+                    <a href="https://templates.iqonic.design/hope-ui/html/dist/dashboard/index.html" class="navbar-brand  d-flex align-items-center">
+                        <!--Logo start-->
+                        <img width="32px" src="<?=base_url()?>img/logo.png" alt="">
+                        <!--logo End--><h5  style="margin-left:5px" class="logo-title text-white" >DakNet</h5>
+                    </a>
+                    <p class="text-white my-4">©
+                        <script type="text/javascript">document.write(new Date().getFullYear())</script> DakNet, All Rights Reserved.
+                    </p>
+
+                </div>
+                <div class="col-md-4 mt-md-0 mt-4">
+                    <h5 class="mb-4 text-white">Kontak</h5>
+                    <div class="d-flex align-items-center mb-4">
+                        <svg width="22" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M22 2H2v20h20V2zM2 6l10 7 10-7"></path>
+                        </svg>
+                        <p class="ms-4 mb-0 text-white">dakwahnetwork@gmail.com</p>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M21 15.9201V18.9201C21.0011 19.1986 20.9441 19.4743 20.8325 19.7294C20.7209 19.9846 20.5573 20.2137 20.3521 20.402C20.1468 20.5902 19.9046 20.7336 19.6407 20.8228C19.3769 20.912 19.0974 20.9452 18.82 20.9201C15.7428 20.5857 12.787 19.5342 10.19 17.8501C7.77382 16.3148 5.72533 14.2663 4.18999 11.8501C2.49997 9.2413 1.44824 6.27109 1.11999 3.1801C1.095 2.90356 1.12787 2.62486 1.21649 2.36172C1.30512 2.09859 1.44756 1.85679 1.63476 1.65172C1.82196 1.44665 2.0498 1.28281 2.30379 1.17062C2.55777 1.05843 2.83233 1.00036 3.10999 1.0001H6.10999C6.5953 0.995321 7.06579 1.16718 7.43376 1.48363C7.80173 1.80008 8.04207 2.23954 8.10999 2.7201C8.23662 3.68016 8.47144 4.62282 8.80999 5.5301C8.94454 5.88802 8.97366 6.27701 8.8939 6.65098C8.81415 7.02494 8.62886 7.36821 8.35999 7.6401L7.08999 8.9101C8.51355 11.4136 10.5864 13.4865 13.09 14.9101L14.36 13.6401C14.6319 13.3712 14.9751 13.1859 15.3491 13.1062C15.7231 13.0264 16.1121 13.0556 16.47 13.1901C17.3773 13.5286 18.3199 13.7635 19.28 13.8901C19.7658 13.9586 20.2094 14.2033 20.5265 14.5776C20.8437 14.9519 21.0122 15.4297 21 15.9201Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                        </svg>
+                        <p class="ms-4 mb-0 text-white">+62 123 4567 890</p>
+                    </div>
+                </div>
+                <div class="col-md-4 mt-md-0 mt-4">
+                    <h5 class="mb-4 text-white">Sosial Media</h5>
+                    <ul class="m-0 p-0 list-unstyled text-white">
+                        <li class="mb-3">Instagram</li>
+                        <li class="mb-3">Facebook</li>
+                    </ul>
+                </div>
+                <hr>
+            </div>
+        </div>
+    </div>
+
+</footer>    <!-- Footer-end -->
+<div id="back-to-top" style="display: none;" class="animate__animated animate__fadeOut">
+    <a class="p-0 btn btn-primary btn-sm position-fixed top" id="top" href="https://templates.iqonic.design/hope-ui/html/dist/landing-pages/blog.html#top">
+        <svg class="icon-30" width="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 15.5L12 8.5L19 15.5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+        </svg>
+    </a>
+</div>
+<a class="btn btn-fixed-end btn-primary btn-icon btn-landing" href="https://templates.iqonic.design/hope-ui/html/dist/dashboard/index.html">
+    Dashboard Demo
+</a>
+
+<script src="<?=base_url()?>assets/js/core/libs.min.js"></script>
+<script src="<?=base_url()?>assets/js/core/external.min.js"></script>
+<script src="<?=base_url()?>assets/js/charts/widgetcharts.js"></script>
+<script src="<?=base_url()?>assets/js/charts/vectore-chart.js"></script>
+<script src="<?=base_url()?>assets/js/charts/dashboard.js" ></script>
+<script src="<?=base_url()?>assets/js/plugins/fslightbox.js"></script>
+<script src="<?=base_url()?>assets/js/plugins/setting.js"></script>
+<script src="<?=base_url()?>assets/js/plugins/slider-tabs.js"></script>
+<script src="<?=base_url()?>assets/js/plugins/form-wizard.js"></script>
+<script src="<?=base_url()?>assets/vendor/aos/dist/aos.js"></script>
+<script src="<?=base_url()?>assets/js/hope-ui.js" ></script>
+
+<script src="<?=base_url()?>assets/js/swiper-bundle.min.js" type="text/javascript"></script>
+<script src="<?=base_url()?>assets/js/app-landing.js" defer="" type="text/javascript"></script>
+
+</body></html>
